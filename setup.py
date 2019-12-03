317724d3e5914376eb1a04aed5abd85701fb5504
Hector Castro <hectcastro@gmail.com>
2019-12-03 09:37:44 -0500

    Fix lint

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     ],
     keywords="django amazon ses email",
     py_modules=["django_amazon_ses"],
-    install_requires=["boto3>=1.3.0", "Django>=1.11,<3.1",],
+    install_requires=["boto3>=1.3.0", "Django>=1.11,<3.1"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 )
