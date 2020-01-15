8dddf47c5bcd7b88e237cd57ed0a0c2b04d4d01e
Hector Castro <hectcastro@gmail.com>
2020-01-15 07:22:20 -0500

    Drop Python 2.7 and 3.4 support

--- setup.py
+++ setup.py
@@ -28,10 +28,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -40,5 +37,5 @@ setup(
     keywords="django amazon ses email",
     py_modules=["django_amazon_ses"],
     install_requires=["boto3>=1.3.0", "Django>=1.11,<3.1"],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 )
